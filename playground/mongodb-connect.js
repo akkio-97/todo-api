2bdef1c41798e55d4b3f2176c79159d8b8222fce
akkio-97 <akshay.nuthala@gmail.com>
2018-05-03 16:30:59 +0530

    okauy

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 
+var user = {name: "nikhi",last: "george" age: 22};
 
-
+var user = {name: "akshay", age: 21};
 var {name} = user;
 //In new branch
 
